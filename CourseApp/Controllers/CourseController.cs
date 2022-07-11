dc98421b4b79263c61de00b4ea87a414c913725a
Vahap Gözenelioğlu <vgozenelioglu@curiesoft.com>
2022-07-11 14:50:11 +0300

    first commit

---
+++ CourseApp/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        public string Index()
+        {
+            return "Course/Index Page";
+        }
+
+        public string List()
+        {
+            return "Course/List Page";
+        }
+        
+    }
+}
