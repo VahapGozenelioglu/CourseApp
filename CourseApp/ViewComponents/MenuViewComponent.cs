daa33257eb88bba8b305fddc48e0b3d161129863
Vahap Gözenelioğlu <gozeneliogluvahap@gmail.com>
2022-07-27 00:44:27 +0300

    Add View Components

---
+++ CourseApp/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CourseApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.ViewComponents
+{
+    public class MenuViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var categories = new List<Category>()
+            {
+                new Category() {Name = "Category 1"},
+                new Category() {Name = "Category 2"},
+                new Category() {Name = "Category 3"}
+            };
+            return View(categories);
+        }
+    }
+}
